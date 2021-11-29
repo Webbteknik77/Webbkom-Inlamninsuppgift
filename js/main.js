87d3b2c8a3e3e9fd0538d46e7966b20c8743e609
Juan Sagrera <j.sagrera77@gmail.com>
2021-11-29 21:33:44 +0100

    Almost done :)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
 
-let url = "https://www.omdbapi.com/?apikey=4c338183&s=batman";
 
-let searchList = document.getElementById("search-list").innerHTML = "data";
+async function fetchLeagues() {
+    let response = await fetch('https://api-football-standings.azharimm.site/leagues');
+    let data = await response.json();
 
-async function fetchJSON() {
-  let response = await fetch(
-    "https://www.omdbapi.com/?apikey=4c338183&s=superman"
-  );
-  let data = await response.json();
-  console.log(data);
+    console.log(data);
+     return data.data
+}
+
+let showLeauges = document.getElementById('showLeauges');
+
+async function fetchLeague(e) {
+    let response = await fetch(`https://api-football-standings.azharimm.site/leagues/${e.target.value}/standings?season=2020&sort=asc
+    .azharimm.site/leagues`);
+
+    let data = await response.json();
+
+    console.log(data);
+    return data.data
+}
+
+async function initForm() {
+    const leagues = await fetchLeagues() 
+
+    console.log(leagues);
+  
+    let selectList = document.getElementById('leagueList')
+   
+    leagues.forEach((league) => {
+        let option = document.createElement("option");
+        option.value = league.id;
+        option.text = league.name;
+        selectList.appendChild(option);
+   });
+
+   selectList.addEventListener('change',fetchLeague);
 
 }
 
-fetchJSON();
+initForm();
+
